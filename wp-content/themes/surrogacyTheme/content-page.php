9c59d38581b115d48f35d00cdfc505a0f8ce473c
Roman-Moiseenko <60714951+Roman-Moiseenko@users.noreply.github.com>
2022-02-21 00:56:49 +0200

    style++(++)

--- wp-content/themes/surrogacyTheme/content-page.php
+++ wp-content/themes/surrogacyTheme/content-page.php
@@ -3,24 +3,18 @@
  * The template used for displaying page content in page.php
  *
  * @package WordPress
- * @subpackage BadJohnny
- * @since BadJohnny 1.0
  */
 ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<?php if ( ! is_page_template( 'page-templates/front-page.php' ) ) : ?>
-			<?php the_post_thumbnail(); ?>
-			<?php endif; ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header>
-
-		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'badjohnny' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-
-			<?php edit_post_link( __( 'Edit', 'badjohnny' ), '<span class="edit-link">', '</span>' ); ?>
-
-	</article><!-- #post -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <?php if (!is_page_template('page-templates/front-page.php')) : ?>
+            <?php the_post_thumbnail(); ?>
+        <?php endif; ?>
+    </header>
+    <div class="entry-content">
+        <?php the_content(); ?>
+        <?php wp_link_pages(array('before' => '<div class="page-links">' . __('Pages:', 'badjohnny'), 'after' => '</div>')); ?>
+    </div><!-- .entry-content -->
+    <?php edit_post_link(__('Edit', 'badjohnny'), '<span class="edit-link">', '</span>'); ?>
+</article><!-- #post -->
--- wp-content/themes/surrogacyTheme/header.php
+++ wp-content/themes/surrogacyTheme/header.php
@@ -19,40 +19,44 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width"/>
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11"/>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
+    <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+    <!--[if lt IE 9]>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<header id="masthead" class="site-header top-header" role="banner">
-		<hgroup>
+    <header id="masthead" class="site-header top-header" role="banner">
+        <hgroup>
             <div>Логотип</div>
-            <div> (Phone) <?= get_option('number_of_main_phone')?>
+            <div> (Phone) <?= get_option('number_of_main_phone') ?>
 
-                <a href="https://wa.me/<?= get_option('whatsapp_phone')?>" rel="nofollow" target="_blank">WhatsApp</a>
-                <a href="<?= get_option('link_facebook')?>" rel="nofollow" target="_blank">Fb</a>
-                <a href="<?= get_option('link_instagram')?>" rel="nofollow" target="_blank">Inst</a>
+                <a href="https://wa.me/<?= get_option('whatsapp_phone') ?>" rel="nofollow" target="_blank">WhatsApp</a>
+                <a href="<?= get_option('link_facebook') ?>" rel="nofollow" target="_blank">Fb</a>
+                <a href="<?= get_option('link_instagram') ?>" rel="nofollow" target="_blank">Inst</a>
             </div>
-		</hgroup>
+        </hgroup>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Menu', 'badjohnny' ); ?></button>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'badjohnny' ); ?>"><?php _e( 'Skip to content', 'badjohnny' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'items_wrap' => '%3$s', 'container' => false) ); ?>
-		</nav><!-- #site-navigation -->
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <button class="menu-toggle"><?php _e('Menu', 'badjohnny'); ?></button>
+            <a class="assistive-text" href="#content"
+               title="<?php esc_attr_e('Skip to content', 'badjohnny'); ?>"><?php _e('Skip to content', 'badjohnny'); ?></a>
+            <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'nav-menu', 'items_wrap' => '%3$s', 'container' => false)); ?>
+        </nav><!-- #site-navigation -->
 
-		<?php if ( get_header_image() ) : ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
-		<?php endif; ?>
-	</header><!-- #masthead -->
+        <?php if (get_header_image()) : ?>
+            <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php header_image(); ?>" class="header-image"
+                                                                 width="<?php echo get_custom_header()->width; ?>"
+                                                                 height="<?php echo get_custom_header()->height; ?>"
+                                                                 alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"/></a>
+        <?php endif; ?>
+    </header><!-- #masthead -->
 
-	<div id="main" class="wrapper">
+    <div id="main" class="wrapper">
