88024348885789f5781f45c06466d0636a7bb383
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2022-02-22 22:31:11 +0200

    style+++++++++++

--- wp-content/themes/surrogacyTheme/header.php
+++ wp-content/themes/surrogacyTheme/header.php
@@ -1,23 +1,11 @@
 <?php
 /**
  * The Header template for our theme
- *
  * Displays all of the <head> section and everything up till <div id="main">
- *
  * @package WordPress
- * @subpackage BadJohnny
- * @since BadJohnny 1.0
  */
 ?><!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
     <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width"/>
