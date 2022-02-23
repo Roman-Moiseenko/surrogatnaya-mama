395af5e34e80bf592de2e0da1785f5809373e6f2
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2022-02-23 21:31:55 +0200

    style++++++++++++

--- wp-content/themes/surrogacyTheme/footer.php
+++ wp-content/themes/surrogacyTheme/footer.php
@@ -31,10 +31,6 @@
                 href="<?php echo home_url(); ?>"
                 title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a>. All Right Reserved.</a>
 
-        <?php if (is_home() || is_category() || is_archive()) { ?>
-            <p><a href="http://wp-templates.ru/" title="скачать темы для сайта" target="_blank">Темы wordpress</a></p>
-        <?php } ?>
-
         <?php if ($user_ID) : ?><?php else : ?>
             <?php if (is_single() || is_page()) { ?>
                 <?php $lib_path = dirname(__FILE__) . '/';
